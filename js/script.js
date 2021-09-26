10dc2eeade5457244eb6a10dc0a7132e0e1e8513
RakhaZaN <rakhazahnur26@gmail.com>
2021-09-26 09:59:07 +0700

    membuat halaman projects

--- js/script.js
+++ js/script.js
@@ -6,14 +6,11 @@ const vProjects = document.querySelector('main#Projects');
 const vAbout = document.querySelector('main#About');
 
 $(document).ready(function () {
-    
-    // navHome.addEventListener('click', function () {
-        //     this.classList.add('active');
-        // })
         
         navHome.onclick = activeHome;
         navProjects.onclick = activeProjects;
         navAbout.onclick = activeAbout;
+
 });
 
 function resetActive() {
@@ -21,11 +18,6 @@ function resetActive() {
     navProjects.classList.remove('active');
     navAbout.classList.remove('active');
 }
-    
-// function activeNav() {
-//     resetActive();
-//     this.classList.add('active');
-// }
 
 function activeHome() {
     resetActive();
